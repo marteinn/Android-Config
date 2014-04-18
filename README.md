291b28cb283fb8594ce49a65bc266091cbff498a
Martin Sandström <martin@marteinn.se>
2014-04-18 11:47:27 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,61 @@
 Android-Config
-==============
+================
+
+Config is a utility class for Android that helps you manage your app configuration.
+
+## How it works
+
+Config loads and parses .properties files placed in the **res/raw** folder of your application (it loads a debug file if debuggable is true), then exposes a class with a set of getParam methods. It's built around the same principle as [Android-Features](https://github.com/marteinn/Android-Features).
+
+## Implementation
+
+First create a folder in your res folder named raw, then place two files there, one for the live build and one for the debug/dev (I usually name them config_app_debug.properties and config_app.properties)
+
+Then open the files and specify your settings using the following format (we parse them with java.util.Properties).
+
+    URL = http://marteinn.se
+    AGE = 3600
+    TRACKING = 1
+    
+(Integer values 1 and 0 can also be treated as booleans).
+
+
+After that you need to include the utility and run createInstance when your app starts, I recommend you place it in application onCreate, like this:
+
+
+    public class YourApplication extends Application {
+        @Override
+        public void onCreate() {
+            Config.createInstance().load(this,
+                R.raw.config_app_debug,
+                R.raw.config_app);
+        }
+    }
+
+You can also skip the validation part and load the configuration file directly.
+
+    public class YourApplication extends Application {
+        @Override
+        public void onCreate() {
+            Config.createInstance().load(this, R.raw.config_app_debug);
+        }
+    }
+
+
+
+
+Then retrive the various settings with these methods:
+
+	Config.getInstance().getParam("URL"); // http://marteinn.se
+	Config.getInstance().getIntParam("AGE"); // 3600
+	Config.getInstance().getBooleanParam("TRACKING"); // true
+	
+	
+## Contributing
+
+Want to contribute? Awesome. Just send a pull request.
+
+
+## License
+
+Android-Features is released under the [MIT License](http://www.opensource.org/licenses/MIT).
---
+++ src/se/marteinn/utils/Config.java
@@ -0,0 +1,137 @@
+package se.marteinn.utils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.content.res.Resources.NotFoundException;
+
+public class Config {
+    public static final String TAG = Config.class.toString();
+
+    // Members
+    private Properties mProperties;
+    private static Config sInstance;
+
+    static public Config getInstance() {
+        return sInstance;
+    }
+
+    /**
+     * Creates singleton class instance.
+     * @param context
+     * @param debugResource
+     * @param liveResource
+     * @return
+     */
+    static public Config createInstance() {
+        sInstance = new Config();
+        return sInstance;
+    }
+
+    /**
+     * Loads and parses config from resource, uses debug resource if debuggable.
+     * @param context
+     * @param debugResource
+     * @param liveResource
+     * @return
+     */
+    public void load(Context context,  int debugResource,
+            int liveResource) {
+
+        int resourceId = 0;
+
+        // Check either live or debug resource
+        resourceId = ! isDebuggable(context) ? liveResource : debugResource;
+        load(context, resourceId);
+    }
+
+    /**
+     * Loads and parses config from resource.
+     * is on.
+     * @param context
+     * @param debugResource
+     * @param liveResource
+     * @return
+     */
+    public void load(Context context, int resourceId) {
+        Properties parsedProperties = null;
+
+        // Load and parse resource
+        try {
+            InputStream rawResource;
+            rawResource = context.getResources().openRawResource(resourceId);
+
+            parsedProperties = new Properties();
+            parsedProperties.load(rawResource);
+        } catch (NotFoundException e) {
+            System.err.println("Could not find iconfig resource: "+e);
+        } catch (IOException e) {
+            System.err.println("Failed to parse config file: "+e);
+        }
+
+        mProperties = parsedProperties;
+    }
+
+    /**
+     * Checks if param exist.
+     * @param name
+     * @return
+     */
+    public Boolean hasParam(String name) {
+        return mProperties.containsKey(name);
+    }
+
+    /**
+     * Get parameter.
+     * @param name
+     * @return
+     */
+    public String getParam(String name) {
+        return (String) mProperties.get(name);
+    }
+
+    /**
+     * Get parameter as int.
+     * @param name
+     * @return
+     */
+    public int getIntParam(String name) {
+        return Integer.parseInt(getParam(name));
+    }
+
+    /**
+     * Get parameter as int.
+     * @param name
+     * @return
+     */
+    public Boolean getBooleanParam(String name) {
+        return getIntParam(name) == 1;
+    }
+
+    /**
+     * Check if app is running with under debug.
+     * Inspiration/code from: http://bit.ly/1gmLMm3
+     * @param context
+     * @return
+     */
+    private boolean isDebuggable(Context context) {
+        boolean debuggable = false;
+        PackageManager pm = context.getPackageManager();
+
+        try {
+            ApplicationInfo appinfo = pm.getApplicationInfo(
+                    context.getPackageName(), 0);
+
+            debuggable = (0 != (appinfo.flags &= ApplicationInfo.FLAG_DEBUGGABLE));
+        } catch(NameNotFoundException e) {
+            /*debuggable variable will remain false*/
+        }
+
+        return debuggable;
+    }
+}
